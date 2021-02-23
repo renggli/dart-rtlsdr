a5a3014bd4e6b91d91394b758e82cff9208657f1
Lukas Renggli <renggli@gmail.com>
2021-02-23 20:38:58 +0100

    Generated files.

---
+++ lib/src/features/frequency.dart
@@ -0,0 +1,37 @@
+import '../ffi/bindings.dart';
+import '../rtlsdr.dart';
+import '../utils/errors.dart';
+
+extension FrequencyExtension on RtlSdr {
+  /// Get the actual frequency in Hz this device is tuned to.
+  int get centerFrequency {
+    RtlSdrException.checkOpen(this);
+    final result = bindings.getCenterFreq(handle);
+    RtlSdrException.checkError(result, 'Failed to get center frequency.');
+    return result;
+  }
+
+  /// Set the actual frequency in Hz the device is tuned to.
+  set centerFrequency(int frequency) {
+    RtlSdrException.checkOpen(this);
+    final result = bindings.setCenterFreq(handle, frequency);
+    RtlSdrException.checkError(
+        result, 'Failed to set center frequency to ${frequency}Hz.');
+  }
+
+  /// Get actual frequency correction value of the device in parts per million.
+  int get frequencyCorrection {
+    RtlSdrException.checkOpen(this);
+    final result = bindings.getFreqCorrection(handle);
+    RtlSdrException.checkError(result, 'Failed to get frequency correction.');
+    return result;
+  }
+
+  /// Set the frequency correction value for the device in parts per million.
+  set frequencyCorrection(int ppm) {
+    RtlSdrException.checkOpen(this);
+    final result = bindings.setFreqCorrection(handle, ppm);
+    RtlSdrException.checkError(
+        result, 'Failed to set frequency correction to ${ppm}ppm.');
+  }
+}
---
+++ lib/src/features/mode.dart
@@ -0,0 +1,38 @@
+import '../ffi/bindings.dart';
+import '../rtlsdr.dart';
+import '../utils/errors.dart';
+
+extension ModeExtension on RtlSdr {
+  /// Enable test mode that returns an 8 bit counter instead of the samples.
+  set testMode(bool enable) {
+    RtlSdrException.checkOpen(this);
+    final result = bindings.setTestmode(handle, enable ? 1 : 0);
+    RtlSdrException.checkError(
+        result, 'Failed to ${enable ? 'enable' : 'disable'} test mode.');
+  }
+
+  /// Enable or disable the internal digital AGC of the RTL2832.
+  set agcMode(bool enable) {
+    RtlSdrException.checkOpen(this);
+    final result = bindings.setAgcMode(handle, enable ? 1 : 0);
+    RtlSdrException.checkError(
+        result, 'Failed to ${enable ? 'enable' : 'disable'} AGC mode.');
+  }
+
+  /// Get enabled state of the offset tuning.
+  bool get offsetTuning {
+    RtlSdrException.checkOpen(this);
+    final result = bindings.getOffsetTuning(handle);
+    RtlSdrException.checkError(result, 'Failed to get offset tuning.');
+    return result == 1;
+  }
+
+  /// Enable or disable offset tuning for zero-IF tuners, which allows to avoid
+  /// problems caused by the DC offset of the ADCs and 1/f noise.
+  set offsetTuning(bool enable) {
+    RtlSdrException.checkOpen(this);
+    final result = bindings.setOffsetTuning(handle, enable ? 1 : 0);
+    RtlSdrException.checkError(
+        result, 'Failed to ${enable ? 'enable' : 'disable'} offset tuning.');
+  }
+}
